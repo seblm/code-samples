c94f1a38a4f79d0abf6fc91004f1658c1b84db8d
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-03-13 22:05:38 +0100

    java 7

--- src/main/java/EchoEnv.java
+++ src/main/java/EchoEnv.java
@@ -1,18 +1,21 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import static java.lang.Runtime.*;
-import static java.lang.System.*;
+import static java.lang.Runtime.getRuntime;
+import static java.lang.System.out;
 
 public class EchoEnv {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(getRuntime().exec("/usr/bin/env").getInputStream()));
-		String currentLine;
-		while ((currentLine = in.readLine()) != null) {
-			if (currentLine.startsWith("PATH")) {
-				out.println(currentLine);
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(getRuntime().exec("/usr/bin/env").getInputStream()))) {
+            String currentLine;
+            while ((currentLine = in.readLine()) != null) {
+                if (currentLine.startsWith("PATH")) {
+                    out.println(currentLine);
+                }
+            }
+        }
+    }
 
 }
