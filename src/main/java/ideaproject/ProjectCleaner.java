3e6d2bd51127a5ad761039dbbfef6abdf2969a42
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-03-13 22:32:43 +0100

    removes idea project files from a directory recursively

---
+++ src/main/java/ideaproject/ProjectCleaner.java
@@ -0,0 +1,33 @@
+package ideaproject;
+
+import java.io.File;
+
+import static java.io.File.separatorChar;
+
+public class ProjectCleaner {
+
+    public static void main(String... args) {
+        if (args.length != 1) {
+            System.err.println("java " + ProjectCleaner.class.getCanonicalName() + " <idea project directory to clean>");
+            System.exit(1);
+        }
+
+        cleanDirectory(new File(args[0]));
+    }
+
+    private static void cleanDirectory(File file) {
+        for (File currentFile : file.listFiles((File pathname) ->
+                pathname.isDirectory() || pathname.isFile() && pathname.getName().endsWith(".iml"))) {
+            if (currentFile.getName().equals(".idea") || currentFile.getName().equals("target")) {
+                System.out.println("rm -fr " + currentFile.getPath() + separatorChar);
+                currentFile.delete(); // TODO doesn't works because directory is not empty
+            } else if (currentFile.isDirectory()) {
+                cleanDirectory(currentFile);
+            } else if (currentFile.getName().endsWith(".iml")) {
+                System.out.println("rm " + currentFile.getPath());
+                currentFile.delete();
+            }
+        }
+    }
+
+}
