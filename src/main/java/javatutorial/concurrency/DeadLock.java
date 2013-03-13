f25c3f2fc6062d30287b51f3884296855c7fbf49
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-03-13 22:05:43 +0100

    java 8

--- src/main/java/javatutorial/concurrency/DeadLock.java
+++ src/main/java/javatutorial/concurrency/DeadLock.java
@@ -13,33 +13,19 @@ public class DeadLock {
         }
 
         public synchronized void bow(Friend bower) {
-            System.out.format("%s: %s"
-                    + " has bowed to me!%n",
-                    this.name, bower.getName());
+            System.out.format("%s: %s has bowed to me!%n", this.name, bower.getName());
             bower.bowBack(this);
         }
 
         public synchronized void bowBack(Friend bower) {
-            System.out.format("%s: %s"
-                    + " has bowed back to me!%n",
-                    this.name, bower.getName());
+            System.out.format("%s: %s has bowed back to me!%n", this.name, bower.getName());
         }
     }
 
     public static void main(String[] args) {
-        final Friend alphonse =
-                new Friend("Alphonse");
-        final Friend gaston =
-                new Friend("Gaston");
-        new Thread(new Runnable() {
-            public void run() {
-                alphonse.bow(gaston);
-            }
-        }).start();
-        new Thread(new Runnable() {
-            public void run() {
-                gaston.bow(alphonse);
-            }
-        }).start();
+        final Friend alphonse = new Friend("Alphonse");
+        final Friend gaston = new Friend("Gaston");
+        new Thread(() -> alphonse.bow(gaston)).start();
+        new Thread(() -> gaston.bow(alphonse)).start();
     }
 }
