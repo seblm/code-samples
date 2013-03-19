97d02f63e4c3b66de422d7d8965317dd90f8b06f
Sébastian Le Merdy <seb@MacBook-Pro-de-Sebastian-2.local>
2013-03-19 23:22:09 +0100

    repair test

--- src/test/java/scanner/ScannerTest.java
+++ src/test/java/scanner/ScannerTest.java
@@ -3,13 +3,14 @@ package scanner;
 import org.junit.Test;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class ScannerTest {
 
     @Test
-    public void test() {
+    public void should_read_6_lines_and_match_pattern() {
         Scanner scanner = new Scanner("2013-01-29 21:57:21,979 WARN com.github.vspiewak.mowitnow.mower.domain.XEngine - scene constraint: 5 5 N can't update at Position{x=5, y=6}\n" +
                 "2013-01-29 21:57:21,979 WARN com.github.vspiewak.mowitnow.mower.domain.XEngine - scene constraint: 5 5 E can't update at Position{x=6, y=5}\n" +
                 "2013-01-29 21:57:21,979 WARN com.github.vspiewak.mowitnow.mower.domain.XEngine - scene constraint: 5 5 E can't update at Position{x=6, y=5}\n" +
@@ -19,10 +20,14 @@ public class ScannerTest {
                 "2013-01-29 21:57:21,979 INFO com.github.vspiewak.mowitnow.mower.app.App - 5 5 E\n" +
                 "0 0 W\n" +
                 "\n").useDelimiter("\n");
+        Pattern mowerPosition = Pattern.compile("com\\.github\\.vspiewak\\.mowitnow\\.mower\\.app\\.App - (.+)");
+        for (Integer i = 0; i < 6; i++) {
+            System.out.println(scanner.nextLine());
+        }
 
-        assertThat(scanner.findInLine("com\\.github\\.vspiewak\\.mowitnow\\.mower\\.app\\.App - (.+)"))
-                .isNotNull()
-                .isEqualTo("com\\.github\\.vspiewak\\.mowitnow\\.mower\\.app\\.App - (.+)");
+        scanner.findInLine(mowerPosition);
+
+        assertThat(scanner.match().group(1)).isEqualTo("5 5 E");
     }
 
 }
