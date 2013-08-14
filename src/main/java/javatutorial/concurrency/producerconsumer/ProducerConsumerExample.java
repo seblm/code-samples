2f7445c8a80eacba58af04fb1e095c14986f1608
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-08-14 06:02:23 +0200

    consumer producer example on concurrency

---
+++ src/main/java/javatutorial/concurrency/producerconsumer/ProducerConsumerExample.java
@@ -0,0 +1,11 @@
+package javatutorial.concurrency.producerconsumer;
+
+public class ProducerConsumerExample {
+
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+
+}
