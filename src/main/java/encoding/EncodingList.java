c5242bfb3fb8b999084426ca4b9b2ecfac87dce2
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-01-06 18:26:24 +0100

    only one encoding list tool

--- src/main/java/encoding/EncodingList.java
+++ src/main/java/encoding/EncodingList.java
@@ -1,19 +1,26 @@
 package encoding;
 
 import java.nio.charset.Charset;
+import java.util.Map;
+
+import static java.nio.charset.Charset.availableCharsets;
 
 public class EncodingList {
 
-    public static void main(String[] args) {
-        for (Charset charset : Charset.availableCharsets().values()) {
-            StringBuilder out = new StringBuilder();
-            out.append(charset.name());
-            out.append('\t').append(charset.displayName());
-            for (String alias : charset.aliases()) {
-                out.append('\t').append(alias);
-            }
-            System.out.println(out.toString());
-        }
-    }
+   public static void main(String[] args) {
+      System.out.println("Charset.forName\tcharset.name\tcharset.displayName\taliases");
+      for (Map.Entry<String, Charset> charset : availableCharsets().entrySet()) {
+         StringBuilder currentCharset = new StringBuilder();
+         currentCharset.append(charset.getKey()).append('\t');
+         currentCharset.append(charset.getValue().name()).append('\t');
+         currentCharset.append(charset.getValue().displayName());
+
+         for (String alias : charset.getValue().aliases()) {
+            currentCharset.append('\t').append(alias);
+         }
+
+         System.out.println(currentCharset.toString());
+      }
+   }
 
 }
