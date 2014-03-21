f996368cbb6dce5339b9e466a907a501ca306fce
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-21 11:25:10 +0100

    display default charset as first

--- src/main/java/encoding/EncodingList.java
+++ src/main/java/encoding/EncodingList.java
@@ -8,19 +8,23 @@ import static java.nio.charset.Charset.availableCharsets;
 public class EncodingList {
 
    public static void main(String[] args) {
-      System.out.println("Charset.forName\tcharset.name\tcharset.displayName\taliases");
-      for (Map.Entry<String, Charset> charset : availableCharsets().entrySet()) {
-         StringBuilder currentCharset = new StringBuilder();
-         currentCharset.append(charset.getKey()).append('\t');
-         currentCharset.append(charset.getValue().name()).append('\t');
-         currentCharset.append(charset.getValue().displayName());
-
-         for (String alias : charset.getValue().aliases()) {
-            currentCharset.append('\t').append(alias);
-         }
-
-         System.out.println(currentCharset.toString());
+      System.out.println("charset.name\tcharset.displayName\taliases");
+      System.out.println(charsetToString(Charset.defaultCharset()));
+      for (Charset charset : availableCharsets().values()) {
+         System.out.println(charsetToString(charset));
       }
+   }
+
+   private static String charsetToString(Charset charset1) {
+      StringBuilder currentCharset = new StringBuilder();
+      currentCharset.append(charset1.name()).append('\t');
+      currentCharset.append(charset1.displayName());
+
+      for (String alias : charset1.aliases()) {
+         currentCharset.append('\t').append(alias);
+      }
+
+      return currentCharset.toString();
    }
 
 }
