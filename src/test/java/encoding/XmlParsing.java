818c7d522bec56bb0156dae842353fffc1612a94
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-02-19 02:21:22 +0100

    avoid warning at compile time and try with resources

--- src/test/java/encoding/XmlParsing.java
+++ src/test/java/encoding/XmlParsing.java
@@ -1,31 +1,29 @@
 package encoding;
 
-import com.sun.org.apache.xerces.internal.impl.io.MalformedByteSequenceException;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.junit.rules.ExpectedException.none;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 public class XmlParsing {
-
-   @Rule
-   public ExpectedException thrown = none();
-
-   @Test
-   public void should() throws ParserConfigurationException, SAXException, IOException {
-      thrown.expect(MalformedByteSequenceException.class);
-      thrown.expectMessage("Octet 1 de la séquence UTF-8 à 1 octets non valide.");
-
-      InputStream file = getClass().getResourceAsStream("/TEST-com.vidal.excalibur.service.resource.RestTestSuite.xml");
-      SAXParserFactory.newInstance().newSAXParser().parse(file, new DefaultHandler());
-   }
-
+    @Test
+    public void should_reproduce_strange_parsing_error_with_given_xml_file() {
+        try (InputStream file = getClass().getResourceAsStream("/TEST-com.vidal.excalibur.service.resource.RestTestSuite.xml")) {
+            try {
+                SAXParserFactory.newInstance().newSAXParser().parse(file, new DefaultHandler());
+            } catch (Throwable thrown) {
+                assertThat(thrown.getClass().getName())
+                        .as("name of the class instead of using isInstanceOf and an import because this import generate a warning during compilation")
+                        .isEqualTo("com.sun.org.apache.xerces.internal.impl.io.MalformedByteSequenceException");
+                assertThat(thrown).hasMessage("Octet 1 de la séquence UTF-8 à 1 octets non valide.");
+            }
+        } catch (IOException e) {
+            fail("", e);
+        }
+    }
 }
