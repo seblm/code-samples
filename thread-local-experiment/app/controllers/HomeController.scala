ea2bc8da642fa7f121346148b7fc29a2ec3b4742
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-11-25 00:04:39 +0100

    simplify controller

--- thread-local-experiment/app/controllers/HomeController.scala
+++ thread-local-experiment/app/controllers/HomeController.scala
@@ -12,33 +12,19 @@ import scala.util.Random
 class HomeController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController with Logging {
 
   def index(id: Option[String]): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    Future(id.map(id => s"$id ").getOrElse(""))
-      .map { id =>
-        logger.info(s"${id}start")
-        id
-      }
-      .map(id => (id, Random.nextLong(5000)))
-      .map { case (id, wait) =>
-        logger.info(s"${id}wait ${wait}ms")
-        (id, wait)
-      }
-      .flatMap { case (id, wait) =>
-        logger.info(s"${id}flatMap ws")
-        ws.url("http://localhost:9000/waitFor")
-          .addQueryStringParameters("millis" -> wait.toString)
-          .get()
-          .map(response => {
-            logger.info(s"${id}status ${response.status} ${response.statusText}")
-            id
-          })
-      }
-      .map(id => logger.info(s"${id}end"))
-      .map(_ => Ok)
+    val idAsString = id.map(id => s"$id ").getOrElse("")
+    val wait = Random.nextLong(5000)
+    logger.info(s"${idAsString}send waitFor ${wait}ms")
+    val request = ws.url("http://localhost:9000/waitFor").addQueryStringParameters("millis" -> wait.toString)
+    request.get().map { response =>
+      logger.info(s"${idAsString}recv ${response.status} ${response.statusText}")
+      Ok
+    }
   }
 
   def waitFor(millis: Long): Action[AnyContent] = Action.async {
-    logger.info(" " * 30 + s"waitFor $millis")
-    Future(Thread.sleep(millis)).map(_ => logger.info(" " * 30 + "done")).map(_ => Ok)
+    logger.info(" " * 9 + s"waitFor ${millis}ms")
+    Future(Thread.sleep(millis)).map(_ => logger.info(" " * 17 + s"${millis}ms done")).map(_ => Ok)
   }
 
 }
