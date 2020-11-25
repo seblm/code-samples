db9a3645c10882aa37279ac8b4d8252c9863dfa2
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2020-11-25 01:35:50 +0100

    id is now mandatory and apply scalafmt

--- thread-local-experiment/app/controllers/HomeController.scala
+++ thread-local-experiment/app/controllers/HomeController.scala
@@ -9,22 +9,26 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
 @Singleton
-class HomeController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController with Logging {
+class HomeController @Inject() (ws: WSClient, val controllerComponents: ControllerComponents)(implicit
+    ec: ExecutionContext
+) extends BaseController
+    with Logging {
 
-  def index(id: Option[String]): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val idAsString = id.map(id => s"$id ").getOrElse("")
-    val wait = Random.nextLong(5000)
-    logger.info(s"${idAsString}send waitFor ${wait}ms")
-    val request = ws.url("http://localhost:9000/waitFor").addQueryStringParameters("millis" -> wait.toString)
-    request.get().map { response =>
-      logger.info(s"${idAsString}recv ${response.status} ${response.statusText}")
-      Ok
+  def index(id: String): Action[AnyContent] =
+    Action.async { implicit request: Request[AnyContent] =>
+      val wait = Random.nextLong(5000)
+      logger.info(s"$id send waitFor ${wait}ms")
+      val request = ws.url(s"http://localhost:9001/wait-for/$id").addQueryStringParameters("millis" -> wait.toString)
+      request.get().map { response =>
+        logger.info(s"$id recv ${response.status} ${response.statusText}")
+        Ok
+      }
     }
-  }
 
-  def waitFor(millis: Long): Action[AnyContent] = Action.async {
-    logger.info(" " * 9 + s"waitFor ${millis}ms")
-    Future(Thread.sleep(millis)).map(_ => logger.info(" " * 17 + s"${millis}ms done")).map(_ => Ok)
-  }
+  def waitFor(id: String, millis: Long): Action[AnyContent] =
+    Action.async {
+      logger.info(s"$id${" " * 9}waitFor ${millis}ms")
+      Future(Thread.sleep(millis)).map(_ => logger.info(s"$id${" " * 17}${millis}ms done")).map(_ => Ok)
+    }
 
 }
