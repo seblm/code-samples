96cd6068516082468752133538eece3978d96604
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2025-12-13 20:58:26 +0100

    selma: remove everything

--- selma-custom-mapper/src/main/java/selma/model/FromCustom.java
+++
@@ -1,9 +0,0 @@
-package selma.model;
-
-public class FromCustom {
-    public final FromInternal internal;
-
-    public FromCustom(FromInternal internal) {
-        this.internal = internal;
-    }
-}
